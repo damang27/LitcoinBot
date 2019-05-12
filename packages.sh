7e50340c6e5823c69b8da15180432181b2f4f781
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-12 22:27:20 +0800

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install python -y
+pkg install python-dev -y
+pkg install nano -y
+pip install --upgrade pip
+
+echo
+echo -e "\e[1m Subscribe to Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Enter :\e[33m bash unzip.sh"
+echo
---
+++ unzip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+clear
+unzip LitecoinBot.zip
+
+echo
+echo -e "\e[1m Subscribe to Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Enter :\e[33m bash Litecoin.sh"
+echo
