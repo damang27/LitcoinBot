704c63448f8658b127c4230382484e3581abdb5e
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-12 23:35:01 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -17,5 +17,7 @@ pip install -r requirements.txt
 echo
 echo -e "\e[1m Subscribe to Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Enter :\e[33m cd LitecoinBot"
+echo -e "\e[1m\e[32m Enter :\e[33m python main.py <country code><10 Digits phone number>"
+echo
+echo -e "\e[1m\e[32m Example :\e[33m python main.py +639072174864"
 echo
