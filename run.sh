d91c7f573f706f33c6f20f73fa905f3513b87101
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-13 00:58:36 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+clear
+ls
+cd LitecoinBot
+
+
+echo
+echo -e "\e[1m Subscribe to Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Enter :\e[33m python main.py +63"
+echo
+echo -e "\e[1m\e[32m Example :\e[33m python main.py +639072175864"
+echo
+echo
