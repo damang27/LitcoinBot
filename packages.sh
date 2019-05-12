457b42bdcb962a6e02549986a96a02b8b2c55271
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-12 23:04:34 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -15,5 +15,5 @@ unzip LitecoinBot.zip
 echo
 echo -e "\e[1m Subscribe to Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Enter :\e[33m bash unzip.sh"
+echo -e "\e[1m\e[32m Enter :\e[33m bash Litecoin.sh"
 echo
