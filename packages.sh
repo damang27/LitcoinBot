3e5c8a3b98c1042e0c0851b377227308b2a4d92f
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-13 01:00:49 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -11,13 +11,12 @@ pkg install python-dev -y
 pkg install nano -y
 pip install --upgrade pip
 unzip LitecoinBot.zip
-cd LitecoinBot
+
 pip install -r requirements.txt
 
 echo
 echo -e "\e[1m Subscribe to Termux Tricks & Tut"
 echo
-echo -e "\e[1m\e[32m Enter :\e[33m python main.py <country code><10 Digits phone number>"
+echo -e "\e[1m\e[32m Enter :\e[33m bash run.sh"
 echo
-echo -e "\e[1m\e[32m Example :\e[33m python main.py +639072174864"
-echo
+
